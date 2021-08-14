25fb6cf62507d4d12f4620eaa3c43936f4dc9fce
housecat <housecatisinthehouse@gmail.com>
2021-08-15 00:03:49 +0200

    Add ChartJS. Create /Chart/ to hold chart abstractions. Create CollateralPieChart and DebtCreditPieChart to showcase examples. Place on Components

---
+++ src/components/composed/DebtCreditPieChart.svelte
@@ -0,0 +1,41 @@
+<script>
+import PieChart from './Charts/PieChart.svelte';
+import tailwind from '../../../tailwind.config';
+
+const CHART_COLORS = {
+  blue6: tailwind.theme.colors.blue6,
+  blue4: tailwind.theme.colors.blue4,
+  blue5: tailwind.theme.colors.blue5,
+};
+
+const legend = [
+  {
+    label: 'Debt',
+    color: CHART_COLORS.blue6,
+    colorName: 'blue6',
+  },
+  {
+    label: 'Credit',
+    color: CHART_COLORS.blue4,
+    colorName: 'blue4',
+  },
+  {
+    label: 'Excess Credit',
+    color: CHART_COLORS.blue5,
+    colorName: 'blue5',
+  },
+];
+
+const title = 'Debt/Credit';
+const subTitle = '(Everything)';
+
+const chartData = [40, 50, 10];
+</script>
+
+<PieChart
+  legend="{legend}"
+  title="{title}"
+  subTitle="{subTitle}"
+  chartData="{chartData}"
+  colors="{Object.values(CHART_COLORS)}"
+/>
