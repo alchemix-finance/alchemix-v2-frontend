31448e0cf880a0d5e19627217e7ef6263bfca2e2
housecat <housecatisinthehouse@gmail.com>
2021-08-22 13:14:12 +0200

    Add storybook. Keep examples in /stories. Add stories for Toast and GasCard

---
+++ src/components/elements/GasCard.stories.svelte
@@ -0,0 +1,12 @@
+<script>
+import { Meta, Template, Story } from '@storybook/addon-svelte-csf';
+import GasCard from './GasCard.svelte';
+</script>
+
+<Meta title="Elements/GasCard" component="{GasCard}" />
+
+<Template let:args>
+  <GasCard {...args} />
+</Template>
+
+<Story name="GasCard" />
