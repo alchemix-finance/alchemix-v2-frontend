58b63d30cfe43aed13276e36d6182f7edcf52d56
housecat <housecatisinthehouse@gmail.com>
2021-09-06 13:02:48 +0200

    remove table from landing

--- src/views/Landing.svelte
+++ src/views/Landing.svelte
@@ -2,7 +2,6 @@
 import Button from '../components/elements/Button.svelte';
 import BorderContainer from '../components/elements/BorderContainer.svelte';
 import AssetCard from '../components/elements/AssetCard.svelte';
-import Table from '../components/composed/Table/Table2.svelte';
 import settings from '../stores/settings';
 import global from '../stores/global';
 import { routerGuard } from '../helpers/routerGuard.js';
@@ -94,4 +93,3 @@ const alAssets = [
     </div>
   {/each}
 </div>
-<Table />
