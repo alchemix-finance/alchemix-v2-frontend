7f8f39d399bcfe7ea30eaa8bcabd73b93c6321b1
housecat <housecatisinthehouse@gmail.com>
2021-09-18 14:53:10 -0400

    Remove comma-number lib and use native JS api

--- src/components/elements/AssetCard.svelte
+++ src/components/elements/AssetCard.svelte
@@ -1,12 +1,12 @@
 <script>
-import commaNumber from 'comma-number';
-
 export let assetName = 'Alchemix Asset';
 export let assetTicker = 'alASS';
 export let assetPrice = 0.01;
 export let currency = '$';
 export let assetIcon = 'icon.png';
 export let assetColor = 'bronze2';
+
+const formattedNumber = new Intl.NumberFormat().format(assetPrice.toFixed(2));
 </script>
 
 <div class="flex flex-col w-80 rounded-3xl bg-grey10 bg-opacity-30 border border-lightgrey20 p-8 pb-0">
@@ -14,7 +14,7 @@ export let assetColor = 'bronze2';
   <p class="text-3xl">{assetTicker}</p>
   <p>
     <span class="text-base opacity-50">{currency}</span>
-    <span class="text-3xl">{commaNumber(assetPrice.toFixed(2))}</span>
+    <span class="text-3xl">{formattedNumber}</span>
   </p>
   <img
     src="images/asset-card-icons/{assetIcon}"
