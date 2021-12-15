6b08e3003a26afab79e3340e7972fd86ad118e96
n4n0GH <n4n0@tuta.io>
2021-12-15 16:37:51 +0100

    contracts can now easily be used within the app

--- src/contracts/AlchemistV2.js
+++
@@ -1,9 +0,0 @@
-import { ethers } from 'ethers';
-
-const setup = require('../abi/AlchemistV2.json');
-
-const abi = setup.abi;
-const provider = new ethers.providers.JsonRpcProvider();
-const signer = provider.getSigner(0);
-
-export default new ethers.Contract(setup.address, abi, signer);
