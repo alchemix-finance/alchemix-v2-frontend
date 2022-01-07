9e57c24e554ef0e8275f0ec29a857fc6b19d11b2
n4n0GH <n4n0@tuta.io>
2022-01-07 08:15:30 +0100

    fixed provided gas in farms

--- src/components/composed/Table/farms/ExpandedFarm.svelte
+++ src/components/composed/Table/farms/ExpandedFarm.svelte
@@ -25,6 +25,7 @@ const updateDepositAmount = (event) => {
 
 const deposit = async () => {
   const amountToWei = utils.parseEther(depositAmount.toString());
+  const gas = utils.parseUnits(getUserGas().toString(), 'gwei');
   if (depositAmount > token.balance) {
     setError('Trying to deposit more than available');
   } else {
@@ -32,7 +33,7 @@ const deposit = async () => {
       let tx;
       setPendingWallet();
       tx = await contract.deposit(poolId, amountToWei, {
-        gasPrice: getUserGas(),
+        gasPrice: gas,
       });
       setPendingTx();
       await provider.once(tx.hash, (transaction) => {
@@ -47,6 +48,7 @@ const deposit = async () => {
 
 const withdraw = async () => {
   const amountToWei = utils.parseEther(withdrawAmount.toString());
+  const gas = utils.parseUnits(getUserGas().toString(), 'gwei');
   if (withdrawAmount > stakedBalance) {
     setError('Trying to withdraw more than available');
   } else {
@@ -54,7 +56,7 @@ const withdraw = async () => {
       let tx;
       setPendingWallet();
       tx = await contract.withdraw(poolId, amountToWei, {
-        gasPrice: getUserGas(),
+        gasPrice: gas,
       });
       setPendingTx();
       await provider.once(tx.hash, (transaction) => {
@@ -68,11 +70,12 @@ const withdraw = async () => {
 };
 
 const claim = async () => {
+  const gas = utils.parseUnits(getUserGas().toString(), 'gwei');
   try {
     let tx;
     setPendingWallet();
     tx = await contract.claim(poolId, {
-      gasPrice: getUserGas(),
+      gasPrice: gas,
     });
     setPendingTx();
     await provider.once(tx.hash, (transaction) => {
@@ -91,34 +94,32 @@ const setDepositValue = (event) => {
 
 const setMaxDeposit = () => {
   depositAmount = token.balance;
-}
+};
 
 const clearDeposit = () => {
-  depositAmount = null
-}
+  depositAmount = null;
+};
 
 const setMaxWithdraw = () => {
   withdrawAmount = stakedBalance;
-}
+};
 
 const clearWithdraw = () => {
   withdrawAmount = null;
-}
-
-
+};
 
 const setWithdrawValue = (event) => {
   // TODO if new value < 1 wei -> withdrawAmount = 1 wei
   withdrawAmount = (parseFloat(stakedBalance) / 100) * event.detail.value;
 };
 
-console.log("TOKEN IN FARM", token)
+console.log('TOKEN IN FARM', token);
 </script>
 
 <!-- NOTE -- the token object is not working at the moment so I had to put in placeholders for styling -->
 
 <div class="grid grid-cols-3 gap-8 pl-8 pr-4 py-4 border-b border-grey10">
-  <div  class="p-4 flex flex-col space-y-4">
+  <div class="p-4 flex flex-col space-y-4">
     <!-- <p class="text-sm text-lightgrey10 self-start">Available</p>
     <div class="w-full self-center">
       <p>{token.balance} {token.symbol}</p>
@@ -161,17 +162,17 @@ console.log("TOKEN IN FARM", token)
       </div>
     </div>
     <Button
-        label="Deposit"
-        borderSize="1"
-        borderColor="green4"
-        backgroundColor="black1"
-        hoverColor="green4"
-        height="h-12"
-        fontSize="text-md"
-        on:clicked="{() => deposit()}"
-      />
+      label="Deposit"
+      borderSize="1"
+      borderColor="green4"
+      backgroundColor="black1"
+      hoverColor="green4"
+      height="h-12"
+      fontSize="text-md"
+      on:clicked="{() => deposit()}"
+    />
   </div>
-  <div  class="p-4 flex flex-col space-y-4">
+  <div class="p-4 flex flex-col space-y-4">
     <!-- <p class="text-sm text-lightgrey10 self-start">Available</p>
     <div class="w-full self-center">
       <p>{token.balance} {token.symbol}</p>
@@ -214,17 +215,17 @@ console.log("TOKEN IN FARM", token)
       </div>
     </div>
     <Button
-        label="Withdraw"
-        borderSize="1"
-        borderColor="green4"
-        backgroundColor="black1"
-        hoverColor="green4"
-        height="h-12"
-        fontSize="text-md"
-        on:clicked="{() => withdraw()}"
-      />
+      label="Withdraw"
+      borderSize="1"
+      borderColor="green4"
+      backgroundColor="black1"
+      hoverColor="green4"
+      height="h-12"
+      fontSize="text-md"
+      on:clicked="{() => withdraw()}"
+    />
   </div>
-<!-- old version for reference -->
+  <!-- old version for reference -->
   <!-- <div class="col-span-1 rounded bg-grey10 w-full">
     <p class="text-sm text-lightgrey10">Staked</p>
     <p>{stakedBalance} {token.symbol}</p>
@@ -232,34 +233,29 @@ console.log("TOKEN IN FARM", token)
     <BalanceQuickSelect on:setInputValue="{setWithdrawValue}" />
     <Button label="Withdraw" on:clicked="{() => withdraw()}" />
   </div> -->
-  <div  class="p-4 flex flex-col space-y-4">
+  <div class="p-4 flex flex-col space-y-4">
     <!-- <p class="text-sm text-lightgrey10 self-start">Available</p>
     <div class="w-full self-center">
       <p>{token.balance} {token.symbol}</p>
     </div> -->
-    <label for="borrowInput" class="text-sm text-lightgrey10">
-      Rewards:
-      </label>
+    <label for="borrowInput" class="text-sm text-lightgrey10"> Rewards: </label>
     <div class="flex bg-grey3 rounded border border-grey3">
       <div class="w-full">
         <div class="w-full rounded appearance-none text-xl text-right h-full py-6 px-14 bg-grey3">
           {unclaimedRewards}
-      {reward}
+          {reward}
         </div>
       </div>
-
-
     </div>
     <Button
-        label="Claim"
-        borderSize="1"
-        borderColor="green4"
-        backgroundColor="black1"
-        hoverColor="green4"
-        height="h-12"
-        fontSize="text-md"
-        on:clicked="{() => claim()}"
-      />
+      label="Claim"
+      borderSize="1"
+      borderColor="green4"
+      backgroundColor="black1"
+      hoverColor="green4"
+      height="h-12"
+      fontSize="text-md"
+      on:clicked="{() => claim()}"
+    />
   </div>
-
 </div>
