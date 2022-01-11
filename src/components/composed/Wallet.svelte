00ae1c05e37263c1ad73d0c3fe6f5b79f83d06ac
n4n0GH <n4n0@tuta.io>
2022-01-11 18:40:01 +0100

    fixes #87

--- src/components/composed/Wallet.svelte
+++ src/components/composed/Wallet.svelte
@@ -1,4 +1,6 @@
 <script>
+import { slide } from 'svelte/transition';
+
 import { connect } from '../../helpers/walletManager';
 import Button from '../elements/Button.svelte';
 import BorderContainer from '../elements/BorderContainer.svelte';
@@ -8,6 +10,7 @@ import account from '../../stores/account';
 import network from '../../stores/network';
 
 let indicatorColor;
+let balanceCollapsed = true;
 
 const debugging = Boolean(parseInt(process.env.DEBUG_MODE, 10));
 
@@ -35,6 +38,10 @@ function openEtherscan() {
   window.open(`https://etherscan.io/address/${$account.address}`, '_blank');
 }
 
+const toggleCollapse = () => {
+  balanceCollapsed = !balanceCollapsed;
+};
+
 $: $network, resolveIndicator($network.id);
 </script>
 
@@ -45,9 +52,9 @@ $: $network, resolveIndicator($network.id);
 }
 </style>
 
-<div class="min-w-wallet">
-  <BorderContainer>
-    {#if $account.signer}
+<BorderContainer>
+  {#if $account.signer}
+    <div class="flex space-x-2">
       <Button
         borderSize="1"
         label="{resolveAddress($account.address)}"
@@ -59,13 +66,48 @@ $: $network, resolveIndicator($network.id);
           <AvatarWithIndicator hash="{$account.address}" bgColor="{indicatorColor}" />
         </div>
       </Button>
-      <WalletBalance />
-    {:else}
-      <Button borderSize="1" label="Connect Wallet" uppercase="{true}" height="h-10" on:clicked="{connect}">
-        <div slot="leftSlot">
-          <AvatarWithIndicator />
-        </div>
+      <Button width="w-max" label="" on:clicked="{() => toggleCollapse()}">
+        <svg
+          slot="rightSlot"
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          {#if balanceCollapsed}
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
+            ></path>
+          {:else}
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21"
+            ></path>
+          {/if}
+        </svg>
       </Button>
+    </div>
+    {#if !balanceCollapsed}
+      <div class="mt-2 py-2 px-4 bg-grey10 rounded" transition:slide>
+        <WalletBalance />
+      </div>
     {/if}
-  </BorderContainer>
-</div>
+  {:else}
+    <Button borderSize="1" label="Connect Wallet" uppercase="{true}" height="h-10" on:clicked="{connect}">
+      <div slot="leftSlot">
+        <AvatarWithIndicator />
+      </div>
+    </Button>
+  {/if}
+</BorderContainer>
