7ff72c82cfd3c691a480be055796d3e6684a5721
chiuzon <chiuzon@protonmail.com>
2022-01-25 15:57:49 +0200

    ☑️ Add update event. Can be usefull when you try to modify the input properties based on value or error

--- src/components/elements/Input.svelte
+++ src/components/elements/Input.svelte
@@ -1,12 +1,25 @@
 <script>
+import { createEventDispatcher } from 'svelte';
+
 export let value;
 export let error;
 export let validator = (val) => [val, undefined];
 
+const dispatch = createEventDispatcher();
+
 let _value;
+let _inputRef;
 
 $: value = validator(_value ?? '')[0];
 $: error = validator(_value ?? '')[1];
+
+$: value,
+  error,
+  dispatch('update', {
+    value: value,
+    error: error,
+    node: _inputRef,
+  });
 </script>
 
-<input {...$$props} bind:value="{_value}" />
+<input {...$$props} bind:value="{_value}" bind:this="{_inputRef}" />
