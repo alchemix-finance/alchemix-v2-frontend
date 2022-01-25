1838dbf44e61bc872d4ea96b63eb81f6499598a5
chiuzon <chiuzon@protonmail.com>
2022-01-25 15:58:51 +0200

    🔍 numericValidator now uses regExp to validate if the value is a number

--- src/helpers/inputValidators.js
+++ src/helpers/inputValidators.js
@@ -1,13 +1,22 @@
+const isNumberRegEx = new RegExp(/^\d+\.?\d*$/);
+
 /**
  * @dev Use the function to check if an input is numeric
  * @param {string} value the input value
  * @returns {[any, string]} [0]: the input value, [1]: string error or undefined
  */
 const numericValidator = (value) => {
-  if (!Number.isNaN(value) && (parseInt(value, 10) > 0 || parseFloat(value) > 0)) {
+  // Returns an empty value and not an error if the input is empty
+  if (value === '') {
+    return ['', undefined];
+  }
+
+  // Return the value just if the conditions are meet
+  if (isNumberRegEx.test(value)) {
     return [value, undefined];
   }
 
+  // Returns an error if the conditions are not meet
   return [undefined, '!VALUE'];
 };
 
