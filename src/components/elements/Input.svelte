e1f73c1f769dbf448603281a8483696870b30532
chiuzon <chiuzon@protonmail.com>
2022-01-25 14:40:27 +0200

    ✒️ Component default validator and error exporting

--- src/components/elements/Input.svelte
+++ src/components/elements/Input.svelte
@@ -1,4 +1,12 @@
 <script>
+export let value;
+export let error;
+export let validator = (val) => [val, undefined];
+
+let _value;
+
+$: value = validator(_value ?? '')[0];
+$: error = validator(_value ?? '')[1];
 </script>
 
-<input {...$props} />
+<input {...$$props} bind:value="{_value}" />
