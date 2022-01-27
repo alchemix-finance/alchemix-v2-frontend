2b18ecc8ef10c17bbe86ff09801b5c45273afca8
chiuzon <89428507+chiuzon@users.noreply.github.com>
2022-01-27 13:12:48 +0200

    Use relative path instead of absolute path

--- scripts/updateAbi.ps1
+++ scripts/updateAbi.ps1
@@ -1,6 +1,8 @@
 
-Remove-Item "$($PWD.Path)\..\src\abi" -Recurse -Force -ErrorAction SilentlyContinue
+Write-Host $($PWD.Path)
+
+Remove-Item ".\src\abi" -Recurse -Force -ErrorAction SilentlyContinue
 git clone git@github.com:alchemix-finance/v2-testnet-artifacts.git;
-New-Item -Type dir -Path "$($PWD.Path)\..\src\abi\"
-Get-ChildItem -Path "$($PWD.Path)\v2-testnet-artifacts\localhost\*" -Include "*.json" | Select-Object -ExpandProperty FullName | Copy-Item  -Destination "$($PWD.Path)\..\src\abi\" -Recurse
-Remove-Item "$($PWD.Path)\v2-testnet-artifacts" -Recurse -Force -ErrorAction SilentlyContinue 
+New-Item -Type dir -Path ".\src\abi\"
+Get-ChildItem -Path ".\v2-testnet-artifacts\localhost\*" -Include "*.json" | Select-Object -ExpandProperty FullName | Copy-Item  -Destination ".\src\abi\" -Recurse
+Remove-Item ".\v2-testnet-artifacts" -Recurse -Force -ErrorAction SilentlyContinue 
