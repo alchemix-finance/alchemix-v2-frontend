17e7d64d1fbd0e4ec065ac6d91107324add83782
chiuzon <89428507+chiuzon@users.noreply.github.com>
2022-02-12 19:28:44 +0200

    Show correct message for type of strategy, count that type of streategies

---
+++ src/stores/v2/vaultActions.ts
@@ -0,0 +1,13 @@
+export async function deposit() {}
+
+export async function depositUnderlying() {}
+
+export async function mint() {}
+
+export async function burn() {}
+
+export async function repay() {}
+
+export async function withdraw() {}
+
+export async function withdrawUnderlying() {}
