4e7a2aa801f416e7dba3462b30f604654b630a29
chiuzon <chiuzon@protonmail.com>
2022-02-20 15:59:00 +0200

    Add transmuter actions

---
+++ src/stores/v2/transmuterActions.ts
@@ -0,0 +1,112 @@
+import { ContractTransaction, ethers, utils } from 'ethers';
+import getUserGas from '@helpers/getUserGas';
+import { contractWrapper, erc20Contract } from '@helpers/contractWrapper';
+import {
+  setPendingWallet,
+  setPendingTx,
+  setSuccessTx,
+  setPendingApproval,
+  setError,
+} from '@helpers/setToast';
+
+export async function deposit(
+  tokenAddress: string,
+  amountToDeposit: ethers.BigNumber,
+  transmuterSelector: string,
+  [signer, addressStore]: [ethers.Signer, string],
+) {
+  try {
+    const gas = utils.parseUnits(getUserGas().toString(), 'gwei');
+    const { instance: transmuterInstance, address: transmuterAddress } = contractWrapper(
+      transmuterSelector,
+      signer,
+    );
+
+    const tokenInstance = erc20Contract(tokenAddress, signer);
+
+    const allowance = await tokenInstance.allowanceOf(addressStore, transmuterAddress);
+
+    if (amountToDeposit.gt(allowance)) {
+      setPendingApproval();
+      await tokenInstance.approve(transmuterAddress);
+    }
+
+    setPendingWallet();
+
+    const tx = (await transmuterInstance.deposit(amountToDeposit, addressStore, {
+      gasPrice: gas,
+    })) as ContractTransaction;
+
+    setPendingTx();
+
+    return await tx.wait().then((transaction) => {
+      setSuccessTx(transaction.transactionHash);
+    });
+  } catch (error) {
+    setError(error.data ? await error.data.message : error.message);
+    console.error(`[vaultActions/deposit]:`, error);
+    throw Error(error);
+  }
+}
+
+export async function withdraw(
+  amountToWithdraw: ethers.BigNumber,
+  transmuterSelector: string,
+  [signer, addressStore]: [ethers.Signer, string],
+) {
+  try {
+    const gas = utils.parseUnits(getUserGas().toString(), 'gwei');
+
+    const { instance: transmuterInstance } = contractWrapper(transmuterSelector, signer);
+    setPendingWallet();
+
+    const tx = (await transmuterInstance.withdraw(amountToWithdraw, addressStore, {
+      gasPrice: gas,
+    })) as ContractTransaction;
+
+    setPendingTx();
+
+    return await tx.wait().then((transaction) => {
+      setSuccessTx(transaction.transactionHash);
+    });
+  } catch (error) {
+    setError(error.data ? await error.data.message : error.message);
+    console.error(`[vaultActions/withdraw]:`, error);
+    throw Error(error);
+  }
+}
+
+export async function claim(
+  underlyingTokenAddress: string,
+  amountToClaim: ethers.BigNumber,
+  transmuterSelector: string,
+  [signer, addressStore]: [ethers.Signer, string],
+) {
+  try {
+    const gas = utils.parseUnits(getUserGas().toString(), 'gwei');
+    const { instance: transmuterInstance } = contractWrapper(transmuterSelector, signer);
+    const dataPackage = new ethers.utils.AbiCoder().encode(['bytes[]'], [[]]);
+
+    setPendingWallet();
+
+    const tx = (await transmuterInstance.claim(
+      amountToClaim,
+      addressStore,
+      [underlyingTokenAddress],
+      dataPackage,
+      {
+        gasPrice: gas,
+      },
+    )) as ContractTransaction;
+
+    setPendingTx();
+
+    return await tx.wait().then((transaction) => {
+      setSuccessTx(transaction.transactionHash);
+    });
+  } catch (error) {
+    setError(error.data ? await error.data.message : error.message);
+    console.error(`[vaultActions/claim]:`, error);
+    throw Error(error);
+  }
+}
