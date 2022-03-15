444eb71c8de2d405376499f125bbe6bb9d69f761
n4n0GH <n4n0@tuta.io>
2022-03-15 10:57:37 +0100

    removed unused imports, added dictionary content

--- src/components/composed/GAlcxWrapper.svelte
+++ src/components/composed/GAlcxWrapper.svelte
@@ -1,13 +1,11 @@
 <script>
-  import { onMount } from 'svelte';
   import { _ } from 'svelte-i18n';
   import ContainerWithHeader from '@components/elements/ContainerWithHeader.svelte';
   import settings from '@stores/settings';
-  import FarmNameCell from '@components/composed/Table/farms/FarmNameCell.svelte';
   import InputNumber from '@components/elements/inputs/InputNumber';
   import ToggleSwitch from '@components/elements/ToggleSwitch';
   import Button from '@components/elements/Button';
-  import { getData, getAllowance, setAllowance, stake, unstake } from '@stores/v2/wrapperActions';
+  import { getData, getAllowance, stake, unstake } from '@stores/v2/wrapperActions';
   import { addressStore, balancesStore } from '@stores/v2/alcxStore';
   import { signer } from '@stores/v2/derived';
   import { utils, BigNumber } from 'ethers';
@@ -95,7 +93,7 @@
 <ContainerWithHeader canToggle="{true}" isVisible="{true}">
   <div slot="header" class="text-sm flex flex-row justify-between">
     <div class="flex flex-row items-center space-x-4">
-      <p>Wrapper</p>
+      <p>{$_('galcx.title')}</p>
     </div>
   </div>
   <div slot="body" class="py-4 px-6 flex space-y-8 flex-col">
@@ -202,12 +200,11 @@
               ? 'border-grey10inverse bg-grey10inverse'
               : 'border-grey10 bg-grey10'} w-full p-4"
           >
-            <p class="mb-4">A liquid ALCX wrapper for single-sided staking.</p>
+            <p class="mb-4">{$_('galcx.leading')}</p>
             <p class="text-sm mb-2">
-              Wrap your ALCX into gALCX and have your ALCX work for you in the single sided staking pool while
-              staying in control over your gALCX.
+              {$_('galcx.p_1')}
             </p>
-            <p class="text-sm">Autocompounding increases the amount of ALCX you can unwrap per gALCX.</p>
+            <p class="text-sm">{$_('galcx.p_2')}</p>
           </div>
 
           <div
@@ -216,13 +213,17 @@
               : 'border-grey10 bg-grey10'} w-full p-4"
           >
             <div class="flex-col">
-              <div class="text-bronze3 mr-2 uppercase text-sm whitespace-nowrap">gALCX Token Supply</div>
+              <div class="text-bronze3 mr-2 uppercase text-sm whitespace-nowrap">
+                {$_('galcx.galcx_supply')}
+              </div>
               <div class="flex">
                 <div class="flex mr-2">{utils.formatEther(totalSupply)}</div>
               </div>
             </div>
             <div class="flex-col">
-              <div class="text-bronze3 mr-2 uppercase text-sm whitespace-nowrap">Wrapped ALCX</div>
+              <div class="text-bronze3 mr-2 uppercase text-sm whitespace-nowrap">
+                {$_('galcx.wrapped_alcx')}
+              </div>
               <div class="flex">
                 <div class="flex mr-2">{utils.formatEther(wrappedSupply)}</div>
               </div>
