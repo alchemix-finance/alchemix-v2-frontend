3391772906185545f1ee0e3ec540457e64314edc
n4n0GH <n4n0@tuta.io>
2022-05-04 07:01:55 +0200

    + add better error handling when no quote could be found on multichain bridging

--- src/middleware/liFi.js
+++ src/middleware/liFi.js
@@ -1,17 +1,23 @@
 import axios from 'axios';
 
 export async function getQuote(fromChain, toChain, fromToken, toToken, fromAmount, fromAddress) {
-  const quote = await axios.get('https://li.quest/v1/quote', {
-    params: {
-      fromChain,
-      toChain,
-      fromToken,
-      toToken,
-      fromAmount,
-      fromAddress,
-    },
-  });
-  return quote.data;
+  await axios
+    .get('https://li.quest/v1/quote', {
+      params: {
+        fromChain,
+        toChain,
+        fromToken,
+        toToken,
+        fromAmount,
+        fromAddress,
+      },
+    })
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => {
+      throw Error(error.response.data.message);
+    });
 }
 
 export async function getStatus(bridge, fromChain, toChain, txHash) {
