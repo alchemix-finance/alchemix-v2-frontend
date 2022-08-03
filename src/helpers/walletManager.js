86877096065fb30b58090b9a777dc4fcd8254826
n4n0GH <n4n0@tuta.io>
2022-08-03 15:16:08 +0200

    Revert "+ update packages"
    This reverts commit a24e0968b5a92f8a80157a1c4be7fd46a3877733.

--- src/helpers/walletManager.js
+++ src/helpers/walletManager.js
@@ -4,12 +4,12 @@ import account from '@stores/account';
 import network from '../stores/network';
 import { uninitData } from './uninitData';
 // import getItl from './getItl';
-import { updateAddress, updateProvider, updateNetwork } from '@stores/v2/methods';
+import { updateAddress, updateProvider, updateNetwork } from '../stores/v2/methods';
 import Onboard from '@web3-onboard/core';
 import injectedModule from '@web3-onboard/injected-wallets';
 import walletConnectModule from '@web3-onboard/walletconnect';
-import coinbaseWallet from '@web3-onboard/coinbase';
-import { chainIds } from '@stores/v2/constants';
+import walletLinkModule from '@web3-onboard/walletlink';
+import { chainIds } from '../stores/v2/constants';
 // import { getRpcUrl } from '../middleware/figment';
 // import {setLoginSuccess} from './setToast'
 
@@ -28,7 +28,7 @@ account.subscribe((val) => {
 
 const injected = injectedModule();
 const walletConnect = walletConnectModule();
-const coinbase = coinbaseWallet();
+const walletLink = walletLinkModule();
 
 // @dev we're literally cheating infura with this lmfao
 const infuraKeys = [
@@ -61,7 +61,7 @@ const supportedChains = chainIds.map((chain) => {
 });
 
 const onboard = Onboard({
-  wallets: [injected, walletConnect, coinbase],
+  wallets: [injected, walletConnect, walletLink],
   chains: supportedChains,
 
   appMetadata: {
@@ -76,9 +76,6 @@ const onboard = Onboard({
   },
   accountCenter: {
     desktop: {
-      enabled: false,
-    },
-    mobile: {
       enabled: false,
     },
   },
--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import nodePolyfills from 'rollup-plugin-polyfill-node';
 import { fileURLToPath } from 'url';
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
 
 const MODE = process.env.NODE_ENV;
 const development = MODE === 'development';
@@ -28,21 +27,6 @@ export default defineConfig({
       '@middleware': fileURLToPath(new URL('./src/middleware', import.meta.url)),
       '@views': fileURLToPath(new URL('./src/views', import.meta.url)),
       '@styles': fileURLToPath(new URL('./src/styles', import.meta.url)),
-    },
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      // Node.js global to browser globalThis
-      define: {
-        global: 'globalThis',
-      },
-      // Enable esbuild polyfill plugins
-      plugins: [
-        NodeGlobalsPolyfillPlugin({
-          process: true,
-          buffer: true,
-        }),
-      ],
     },
   },
   build: {
