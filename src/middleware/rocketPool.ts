e80b5e0a7b89261840fc4057866b4abdf4a4b21c
n4n0 <n4n0@tuta.io>
2022-12-23 17:23:15 +0100

    hotfix for failing api request on rocketpool

--- src/middleware/rocketPool.ts
+++ src/middleware/rocketPool.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const apiUrl = 'https://stake.rocketpool.net/api/mainnet/network/stats';
+const apiUrl = 'https://api.rocketpool.net/api/mainnet/network/stats';
 
 function getApr(
   totalAtStake = 1e6,
