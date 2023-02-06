aad47c7cff6e23c46c8a6ef9146b990d465db573
n4n0 <n4n0@tuta.io>
2023-02-06 13:09:52 +0100

    flarum post cards now use proper regex to visualize aip numbers

--- src/components/composed/FlarumCard.svelte
+++ src/components/composed/FlarumCard.svelte
@@ -12,6 +12,10 @@
   const openOnForum = (url) => {
     window.open(url, '_blank');
   };
+
+  const titleRegEx = (title) => {
+    return title.match(/\[.*?]/g)[0];
+  };
 </script>
 
 <div
@@ -20,7 +24,7 @@
     : 'bg-grey10 border-grey3'}"
 >
   <div class="flex flex-col p-2 mb-10 w-52 lg:w-full flex-1">
-    <p class="mb-2 text-xs lg:text-base flex-1">{postData.title.slice(8)}</p>
+    <p class="mb-2 text-xs lg:text-base flex-1">{titleRegEx(postData.title)}</p>
     <div class="flex flex-row justify-between text-xs lg:text-sm">
       <p class="text-lightgrey5">Created:</p>
       <p>{toDate(postData.created)}</p>
@@ -37,7 +41,7 @@
 
   <div class="w-full p-1">
     <Button
-      label="Read {postData.title.slice(0, 8)}"
+      label="Read {titleRegEx(postData.title)}"
       borderColor="green4"
       backgroundColor="{$settings.invertColors ? 'green7' : 'black2'}"
       hoverColor="green4"
