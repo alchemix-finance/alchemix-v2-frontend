df28b66ae9df138a44e1a2dfc4e89c607cae233d
n4n0 <n4n0@tuta.io>
2023-11-21 12:37:06 +0100

    - turn off unused var lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -29,6 +29,7 @@ module.exports = {
     '@typescript-eslint/no-shadow': 'off',
     '@typescript-eslint/ban-ts-comment': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
   },
   parserOptions: {
     sourceType: 'module',
